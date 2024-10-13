7db0b3973c33d6ac0600553d380b497bbd213274
Keksch <53422233+Keksch@users.noreply.github.com>
2024-10-13 20:21:48 +0200

    Update for WS24
    * Update README.md
    
    * Delete components directory
    
    * Delete layouts directory
    
    * Delete pages directory
    
    * Delete plugins directory
    
    * Delete public directory
    
    * Delete server directory
    
    * Delete .gitignore
    
    * Delete .npmrc
    
    * Delete app.vue
    
    * Delete nuxt.config.ts
    
    * Delete package.json
    
    * Delete tsconfig.json
    
    * Delete yarn.lock
    
    * Add files via upload
    
    * remove dist
    
    * Revert "remove dist"
    
    This reverts commit 30f7bc6832b461007f674d4aeb17f0efa2ab7395.
    
    ---------
    
    Co-authored-by: Duda <sebastian.duda@fit.fraunhofer.de>
    Co-authored-by: duda <sebastian.duda@uni-bayreuth.de>

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center" style="height: 100vh">
+      <v-col cols="12" md="4">
+        <v-card class="pa-4" elevation="2">
+          <v-card-title class="title text-center">
+            Login
+          </v-card-title>
+
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-text-field
+                v-model="name"
+                label="Name"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+
+              <v-btn
+                :disabled="!valid"
+                color="primary"
+                block
+                @click="submitLogin"
+              >
+                Login
+              </v-btn>
+
+              <v-alert v-if="errorMessage" type="error" class="mt-3">
+                {{ errorMessage }}
+              </v-alert>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '', // Store the user's name
+      password: '', // Store the user's password
+      valid: true,
+      errorMessage: '',
+    };
+  },
+  methods: {
+    submitLogin() {
+      if (this.password !== 'eyJhbGci') {
+        this.errorMessage = 'Incorrect password. Please try again.';
+      } else {
+        this.$emit('login', { name: this.name, password: this.password }); // Emit both name and password
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  font-weight: bold;
+  color: #333;
+}
+</style>
